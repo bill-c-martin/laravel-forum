c35abea38bf535e50dd081878baea6e48f27fbca
Bill Martin <bill@billmartin.io>
2018-03-23 03:50:37 +0000

    Add replies to threads

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,17 @@
                 </div>
             </div>
         </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach ($thread->replies as $reply)
+                    <div class="card">
+                        <div class="card-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
