464a93f2174ad83ccc79f694a0bd63cfe4b2306d
Bill Martin <bill@billmartin.io>
2018-03-23 04:22:46 +0000

    Link owner to replies and add reply date

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-16">
                 <div class="card">
                     <div class="card-header">{{ $thread->title }}</div>
 
@@ -15,9 +15,14 @@
         </div>
 
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-16">
                 @foreach ($thread->replies as $reply)
-                    <div class="card">
+                    <div class="card mt-4">
+                        <div class="card-header">
+                            <a href="#">
+                                {{ $reply->owner->name }}
+                            </a> said {{ $reply->created_at->diffForHumans() }}...
+                        </div>
                         <div class="card-body">
                             {{ $reply->body }}
                         </div>
