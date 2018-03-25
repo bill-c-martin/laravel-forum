35f2c526421340e91d07044fbc12f26f9e8be36a
Bill Martin <bill@billmartin.io>
2018-03-25 05:38:50 +0000

    Extract replies from thread view to a partial

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -17,16 +17,7 @@
         <div class="row justify-content-center">
             <div class="col-md-16">
                 @foreach ($thread->replies as $reply)
-                    <div class="card mt-4">
-                        <div class="card-header">
-                            <a href="#">
-                                {{ $reply->owner->name }}
-                            </a> said {{ $reply->created_at->diffForHumans() }}...
-                        </div>
-                        <div class="card-body">
-                            {{ $reply->body }}
-                        </div>
-                    </div>
+                    @include ('threads.reply')
                 @endforeach
             </div>
         </div>
