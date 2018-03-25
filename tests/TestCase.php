6558503b4f4567f3e2d192fc99dbc74a43d9f1d1
Bill Martin <bill@billmartin.io>
2018-03-25 06:31:34 +0000

    Add standard user sign in method for all phpunit tests to use

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Sign in a user
+     *
+     * @param \App\User $user
+     */
+    protected function signIn(User $user)
+    {
+        $this->be($user);
+    }
 }
