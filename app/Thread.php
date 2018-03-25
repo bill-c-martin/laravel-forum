c411677bd7780a77ae0e31750d3563ac76e96c3b
Bill Martin <bill@billmartin.io>
2018-03-25 05:40:49 +0000

    Add creator user to Thread

--- app/Thread.php
+++ app/Thread.php
@@ -6,8 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    /**
+     * Replies to this thread
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    /**
+     * User who created this thread
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
