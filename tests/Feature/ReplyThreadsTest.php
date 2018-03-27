555ce259efba74a1aa782d7a4db40fb486a898d3
Bill Martin <bill@billmartin.io>
2018-03-27 04:20:21 +0000

    Add reply field for auth users, sign in link for guests

--- tests/Feature/ReplyThreadsTest.php
+++ tests/Feature/ReplyThreadsTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ParticipateInForumTest extends TestCase
+class ReplyThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
