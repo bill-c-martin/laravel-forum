3316ad140fa1446e7bf761d9f29d66b9115632b5
Bill Martin <bill@billmartin.io>
2018-04-30 23:44:27 -0400

    Refactor reply thread tests to read better

--- tests/Feature/ReplyThreadsTest.php
+++ tests/Feature/ReplyThreadsTest.php
@@ -14,21 +14,27 @@ class ReplyThreadsTest extends TestCase
     /** @test */
     public function an_unauthenticated_user_cannot_add_replies_to_threads()
     {
+        // Given we have an unauthenticated user
         $this->expectException('Illuminate\Auth\AuthenticationException');
 
-        $dummy_thread_id = $dummy_channel_id = 1;
-        $dummy_reply = [];
+        // And an existing thread
+        $thread = create('App\Thread');
+
+        // When the user adds a reply to the thread
+        $reply = create('App\Reply');
 
         $this->post(
-            route('replies.store', [$dummy_channel_id, $dummy_thread_id]),
-            $dummy_reply
+            route('replies.store', [$thread->channel->id, $thread->id]),
+            $reply->toArray()
         );
+
+        // Then an exception should be thrown
     }
 
     /** @test */
     public function an_authenticated_user_can_add_replies_to_threads()
     {
-        // Given we have an auth user
+        // Given we have an authenticated user
         $this->signIn();
 
         // And an existing thread
@@ -36,6 +42,7 @@ class ReplyThreadsTest extends TestCase
 
         // When the user adds a reply to the thread
         $reply = create('App\Reply');
+
         $this->post(
             route('replies.store', [$thread->channel->id, $thread->id]),
             $reply->toArray()
